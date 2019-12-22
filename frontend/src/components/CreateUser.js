7e56b478b53d82fb2eaed7e33291f7a5d07a2c9e
Roberto E. M. Austin <0315108515@miutt.edu.mx>
2019-12-21 23:49:57 -0800

    Se agrega axios y se consume un api el cual muestra los usuarios dados de alta en pantalla y se guarda en un estado la informacion que trae el api y la que se iria hacia otra api para guardar usuarios

--- frontend/src/components/CreateUser.js
+++ frontend/src/components/CreateUser.js
@@ -1,11 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUser extends Component {
+
+    state = {
+        users:[],
+        username:''
+    }
+
+    async componentDidMount()
+    {
+        const res = await axios.get('http://localhost:4000/api/users');
+        this.setState({users: res.data.users});
+        //Pull Request al repositorio de fazt https://github.com/FaztWeb/mern-crud-2019
+    };
+    
+    onChangeUserName = (e) =>{
+        this.setState({username: e.target.value}); 
+    }
+    
+
     render() {
         return (
-            <div>
-                CreateUser
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="card card-body">
+                        <h3>Create new user</h3>
+                        <form action="">
+                            <div className="form-group">
+                                <input type="text" className="form-control" onChange={this.onChangeUserName}/>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className="col-md-8">
+                    <ul className="list-group">
+                    {
+                        this.state.users.map(user => (<li key={user._id} className="list-group-item list-group-item-action">{user.username}</li>))
+                    }
+                    </ul>
+                </div>
             </div>
-        )
-    }
-}
+        );
+    };
+};
